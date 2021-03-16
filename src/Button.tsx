23bbe8732045d473860831c953d9fc7da0f108d7
Lykke27 <usachev97@gmail.com>
2021-03-16 22:42:52 +0300

    add components and classnames library

---
+++ src/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classNames from 'classnames'
+
+type PropsType = {
+    children: string
+    outline?: boolean
+}
+
+export const Button = (props: PropsType) => {
+    return (
+        <button className={classNames('button', {
+            'button--outline': props.outline
+        })}> {props.children} </button>
+    )
+}
